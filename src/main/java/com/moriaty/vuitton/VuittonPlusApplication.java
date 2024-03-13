86cdc5c3a44adde78dac54180c59febe464dae95
Moriaty <971896352@qq.com>
2024-03-14 00:26:23 +0800

    增加注释

--- src/main/java/com/moriaty/vuitton/VuittonPlusApplication.java
+++ src/main/java/com/moriaty/vuitton/VuittonPlusApplication.java
@@ -4,6 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author Moriaty
+ * @since 2024/3/14 0:01
+ */
 @SpringBootApplication
 @MapperScan("com.moriaty.vuitton.dao.mapper")
 public class VuittonPlusApplication {
